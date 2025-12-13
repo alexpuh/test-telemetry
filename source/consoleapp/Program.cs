578af8c8298580c9a7c4660b4ba39d6eba6eb1ce
alexpuh <alexpuh@googlemail.com>
2025-12-13 22:36:05 +0100

    Call OTel from console app

--- source/consoleapp/Program.cs
+++ source/consoleapp/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System.Diagnostics;
+
+// Create ActivitySource for tracing
+var activitySource = new ActivitySource("ConsoleApp");
+
+// Configure OpenTelemetry
+using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("consoleapp"))
+    .AddSource("ConsoleApp")
+    .AddOtlpExporter(options =>
+    {
+        options.Endpoint = new Uri("http://localhost:4317");
+    })
+    .Build();
+
+// Start a span for the main processing
+using var activity = activitySource.StartActivity("ProcessingOperation", ActivityKind.Internal);
+
+activity?.SetTag("operation.type", "main");
+activity?.AddEvent(new ActivityEvent("Start processing"));
+
+// Simulate some work
+await Task.Delay(100);
+
+activity?.AddEvent(new ActivityEvent("End processing"));
+activity?.SetStatus(ActivityStatusCode.Ok);
+activity?.SetTag("telemetry.exported", "true");
